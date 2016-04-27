50920c1a210a2def72c84a6b1acd7dae539a4358
romavov <rom.vovna@gmail.com>
2016-04-27 23:52:11 +0300

    *add actions: index | show | new | create | edit | update *add gems: bootstrap-sass | simple_form

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,49 @@
+class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @projects = Project.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to project_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+end
